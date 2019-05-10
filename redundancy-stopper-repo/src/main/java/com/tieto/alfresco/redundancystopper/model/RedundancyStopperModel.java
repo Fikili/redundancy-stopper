8b7dd599c6f7dc5bf8ef9c138010e84f2e92337b
Filip Bruska <filip.bruska@tieto.com>
2019-05-10 18:43:44 +0200

    Added model for associations

--- redundancy-stopper-repo/src/main/java/com/tieto/alfresco/redundancystopper/model/RedundancyStopperModel.java
+++ redundancy-stopper-repo/src/main/java/com/tieto/alfresco/redundancystopper/model/RedundancyStopperModel.java
@@ -7,5 +7,8 @@ public class RedundancyStopperModel {
 	public static final String TIETO_MODEL_1_0_URI = "http://www.tieto.com/model/content/1.0";
 	public static final QName ASPECT_REDUNDANCY_IDENTIFICATOR =  QName.createQName(TIETO_MODEL_1_0_URI,"identificator");
 	public static final QName PROP_FINGERPRINT =  QName.createQName(TIETO_MODEL_1_0_URI,"fingerPrint");
+	public static final QName ASPECT_DUPLICATES =  QName.createQName(TIETO_MODEL_1_0_URI,"duplicates");
+	public static final QName PROP_DUPLICATES =  QName.createQName(TIETO_MODEL_1_0_URI,"duplicateAssocs");
+	
 	
 }
